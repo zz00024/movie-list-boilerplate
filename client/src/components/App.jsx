39eb332b34a6a9afaedbaf0a8d3b597c5a21b15a
chienchih <chien@xiaoxianroude-MacBook-Pro.local>
2019-09-30 22:25:45 -0700

    step2: completed except the button function on adding new movies

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -17,13 +17,23 @@ class App extends React.Component {
     };
     this.handleClick = this.handleClick.bind(this);
     this.handleKeyUp = this.handleKeyUp.bind(this);
-
+    this.addMovieButton = this.addMovieButton.bind(this);
   }
 
   handleClick() {
     console.log('clicked');
   }
 
+  addMovieButton() {
+    console.log('add movie clicked');
+    //add user input movie into the table and display it
+    var input, userInput;
+    input = document.getElementById('myMovie');
+    userInput = input.value.toUpperCase();
+
+  }
+
+  //Filter the display table by the user input
   handleKeyUp() {
     var input, filter, table, tr, th;
     input = document.getElementById('myInput');
@@ -46,9 +56,16 @@ class App extends React.Component {
     return (
       <div>
         <h1>Movie List</h1>
-        <input type="text" id="myInput" className = 'addInput' onKeyUp = {this.handleKeyUp} placeholder="Search.."></input>
 
+        <div className = 'addMovie'>
+        <input type="text" id="myMovie" className = 'addMovie' onKeyUp = {this.handleKeyUp} placeholder="Add movie title here..."></input>
+        <button className = 'addMovieButton' onClick = {this.addMovieButton}>Add</button>
+        </div>
+
+        <div className = 'searchMovie'>
+        <input type="text" id="myInput" className = 'addInput' onKeyUp = {this.handleKeyUp} placeholder="Search.."></input>
         <button className = 'button' onClick = {this.handleClick}>GO!</button>
+        </div>
 
         <MovieList movies = {this.state.movies}/>
       </div>
